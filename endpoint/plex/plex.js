d43f830e0b98f91d2fe549ef8c9c9e293bf07a0e
Joshua Noakes <joshua.noakes1@icloud.com>
2021-05-16 18:41:17 +0100

    new updte

--- endpoint/plex/plex.js
+++ endpoint/plex/plex.js
@@ -26,28 +26,38 @@ router.post('/', async function (req, res, next) {
         return;
     }
 
-    // downloading image from tautulli
+    // tautulli stuff
     var tautulli_poster_data = await fetch(req.body.media.playback.poster_url);
     var thumb = await tautulli_poster_data.buffer();
+    var episode_name = req.body.media.playback.episode;
 
     switch (req.body.media.type) {
         case ('episode'):
-            // get episode thumb from TMDB
-            var tmdb_thumb_api_data = await fetch(`https://api.themoviedb.org/3/tv/${req.body.media.ID}/season/${req.body.media.playback.season_number}/episode/${req.body.media.playback.episode_number}/images?api_key=${process.env.TMDB_API_Key}`);
-            var tmdb_thumb_api_json = await tmdb_thumb_api_data.json();
+            // get episode data from tmdb
+            var tmdb_episode = await fetch(`https://api.themoviedb.org/3/tv/${req.body.media.ID}/season/${req.body.media.playback.season_number}/episode/${req.body.media.playback.episode_number}?api_key=${process.env.TMDB_API_Key}`);
+            tmdb_episode = await tmdb_episode.json();
 
-            // download thumb if not we use the tautulli one
-            if (tmdb_thumb_api_json.success != false && tmdb_thumb_api_json.stills.length > 0) {
-                var tmdb_thumb_origin_data = await fetch(`https://www.themoviedb.org/t/p/original/${tmdb_thumb_api_json.stills[0].file_path}`);
-                var thumb = await tmdb_thumb_origin_data.buffer();
+            // if tmdb has the episode 
+            if (tmdb_episode.success != false) {
+                // check episode name
+                if (tmdb_episode.name != req.body.media.playback.episode) {
+                    // use tmdb name for ep when is missing from plex
+                    episode_name = tmdb_episode.name
+                }
+                if (tmdb_episode.still_path != null) {
+                    // download thumb
+                    var tmdb_thumb_origin_data = await fetch(`https://www.themoviedb.org/t/p/original/${tmdb_episode.still_path}`);
+                    var thumb = await tmdb_thumb_origin_data.buffer();
+                }
             }
 
+            // tweet data
             var data = {
                 "media": {
                     "type": "watching",
                     "show": req.body.media.playback.name,
                     "seaepi": `Season ${req.body.media.playback.season_number} Episode ${req.body.media.playback.episode_number}`,
-                    "name": req.body.media.playback.episode
+                    "name": episode_name
                 }
             }
 
