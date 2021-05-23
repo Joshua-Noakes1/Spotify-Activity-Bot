4d54a6cc8b125d6bf1b5e7cd7f51da0a6158a3c6
Joshua Noakes <joshua.noakes1@icloud.com>
2021-05-23 13:58:28 +0100

    spotify uses the twitter function now

--- endpoint/spotify/spotify.js
+++ endpoint/spotify/spotify.js
@@ -3,16 +3,7 @@
 require('dotenv').config()
 const express = require('express');
 const router = express.Router();
-const Twitter = require('twitter');
-
-// twitter config
-var client = new Twitter({
-    consumer_key: process.env.API_Key,
-    consumer_secret: process.env.API_Secret_Key,
-    access_token_key: process.env.Access_Token,
-    access_token_secret: process.env.Access_Token_Secret
-});
-
+const twitter = require('../tweet/tweet');
 
 // /hook post
 router.post('/', (req, res, next) => {
@@ -32,25 +23,11 @@ router.post('/', (req, res, next) => {
         "TrackUrl": req.body.TrackUrl
     }
 
-    // Post to twitter
-    client.post('statuses/update', {
-        status: `Joshua played "${data.TrackName}" by ${data.ArtistName} on ${data.PlayedAt} ${data.TrackUrl}`
-    }, function (error, tweet, response) {
-        // error message
-        if (error) {
-            console.error(error);
-            res.status(500).json({
-                "message": error.message
-            });
-            return;
-        }
+    // send tweet
+    twitter.uploadTweet(`Joshua played "${data.TrackName}" by ${data.ArtistName} on ${data.PlayedAt} ${data.TrackUrl}`, res);
 
-        console.log('Successfuly Posted Tweet')
-        // success message
-        res.status(200).json({
-            "message": "Successfuly posted tweet"
-        });
-    });
+    // success
+    res.status(200).end();
 });
 
 module.exports = router;
