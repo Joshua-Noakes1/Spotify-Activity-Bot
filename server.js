7aa04456c83cd671ec5099a9189aeef6744979bc
Joshua Noakes <joshua.noakes1@icloud.com>
2021-11-20 02:15:43 +0000

    fixed env to config

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 require("dotenv").config();
+require("./lib/config");
 const lcl = require("cli-color");
 const app = require("./app");
 var http = require("http").Server(app);
-const { setupConfig } = require("./lib/config");
 const { socketio } = require("./lib/sockets");
 
-(async () => {
-  await socketio(http);
-  await setupConfig();
+// load websockets
+socketio(http);
 
-  // defaults to port 3000 if no port can be found
-  const port = process.env.port || 3000;
+// defaults to port 3000 if no port can be found
+const port = process.env.port || 3000;
 
-  // create server
-  http.listen(port);
+// create server
+http.listen(port);
 
-  // log server start
-  console.log(
-    lcl.blue("[Info]"),
-    `Started server on port`,
-    lcl.green(`${port}`)
-  );
-})();
+// log server start
+console.log(lcl.blue("[Info]"), `Started server on port`, lcl.green(`${port}`));
