e15a5281f8f4667a11df50dc2a6739a437046fec
Joshua Noakes <joshua.noakes1@icloud.com>
2022-06-12 16:19:46 +0100

    add image not found

--- api/v3/spotify/image.js
+++ api/v3/spotify/image.js
@@ -10,12 +10,18 @@ const
 const router = express.Router();
 
 // Api routes
+router.get('/', function (req, res) {
+    return res.status(404).json({
+        success: false,
+        message: "Image not found."
+    });
+});
 router.get('/:id', async function (req, res) {
     if (existsSync(path.join(__dirname, '../', '../', '../', 'data', 'images', `${req.params.id}.png`))) {
-        console.log(lcl.blue('[Image - Info]'), "Image already exists, sending...");
-        var localImage = await readFileSync(path.join(__dirname, '../', '../', '../', 'data', 'images', `${req.params.id}.png`));
+        console.log(lcl.blue('[Image - Info]'), `Found image ${data.params.id}, sending...`);
+        var image = await readFileSync(path.join(__dirname, '../', '../', '../', 'data', 'images', `${req.params.id}.png`));
         res.contentType('image/png');
-        return res.send(localImage);
+        return res.send(image);
     } else {
         return res.status(404).json({success: false, message: "Image not found."});
     }
