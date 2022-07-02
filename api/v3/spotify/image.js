a1f1cbb9486111237a84321ae74dea8cc457e973
Joshua Noakes <joshua.noakes1@icloud.com>
2022-07-02 21:08:36 +0100

    add font on navbar and cache to images

--- api/v3/spotify/image.js
+++ api/v3/spotify/image.js
@@ -4,7 +4,8 @@ const
     express = require("express"),
     {
         readFileSync,
-        existsSync
+        existsSync,
+        statSync
     } = require("fs");
 // global express router
 const router = express.Router();
@@ -20,7 +21,12 @@ router.get("/:id.png", async function (req, res) {
     if (existsSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`))) {
         console.log(lcl.blue("[Image - Info]"), `Found image "${req.params.id}", sending...`);
         var image = await readFileSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`));
+        var imageStats = await statSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`));
+
+        // send image
         res.contentType("image/png");
+        res.header("Cache-Control", "public, max-age=31536000");
+        res.header("Last-Modified", imageStats.mtime);
         return res.send(image);
     } else {
         console.log(lcl.yellow("[Image - Warn]"), `Image not found "${req.params.id}"`);
