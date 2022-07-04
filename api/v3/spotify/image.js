6cb7fbbd9d270b19ba7b269edf11bcbd61ed05d5
Joshua Noakes <joshua.noakes1@icloud.com>
2022-07-05 00:12:38 +0100

    update how images are served to remove logic

--- api/v3/spotify/image.js
+++ api/v3/spotify/image.js
@@ -1,38 +1,10 @@
-const
-    lcl = require("cli-color"),
-    path = require("path"),
-    express = require("express"),
-    {
-        readFileSync,
-        existsSync,
-        statSync
-    } = require("fs");
+const path = require("path"),
+    express = require("express");
+
 // global express router
 const router = express.Router();
 
 // Api routes
-router.get("/", function (req, res) {
-    return res.status(404).json({
-        success: false,
-        message: "Image not found."
-    });
-});
-router.get("/:id.png", async function (req, res) {
-    if (existsSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`))) {
-        console.log(lcl.blue("[Image - Info]"), `Found image "${req.params.id}", sending...`);
-        var image = await readFileSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`));
-        var imageStats = await statSync(path.join(__dirname, "../", "../", "../", "data", "images", `${req.params.id}.png`));
-
-        // send image
-        res.contentType("image/png");
-        res.header("Cache-Control", "public, max-age=31536000");
-        res.header("Last-Modified", imageStats.mtime);
-        return res.send(image);
-    } else {
-        console.log(lcl.yellow("[Image - Warn]"), `Image not found "${req.params.id}"`);
-        return res.status(404).json({success: false, message: "Image not found."});
-    }
-});
-
+router.use("/", express.static(path.join(__dirname, "../", "../", "../", "data", "images"), {cacheControl: true, etag: true, lastModified: true, maxAge: 25200000}));
 
 module.exports = router;
